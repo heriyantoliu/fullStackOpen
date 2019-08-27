3a8abc110b80122011ed928022afc4d1ef102b4e
heriyanto <heriyanto.liu@sidomuncul.co.id>
2019-08-27 15:46:54 +0700

    exercise 2.12

---
+++ part2/country/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ListCountry = ({ countries }) => {
+	const countriesLength = countries.length;
+	if (countriesLength > 10) {
+		return <p>Too many matches, specify another filter</p>;
+	} else if (countriesLength !== 1) {
+		return countries.map(country => (
+			<div key={country.name}>{country.name}</div>
+		));
+	}
+	return <Country country={countries[0]} />;
+};
+
+const Country = ({ country }) => {
+	return (
+		<div>
+			<h2>{country.name}</h2>
+			<p>Capital: {country.capital}</p>
+			<p>Population: {country.population}</p>
+			<h2>Languages:</h2>
+			<ul>
+				{country.languages.map(language => (
+					<li key={language.iso639_1}>{language.nativeName}</li>
+				))}
+			</ul>
+			<img src={country.flag} height='100' width='100' />
+		</div>
+	);
+};
+
+const App = () => {
+	const [countries, setCountries] = useState([]);
+	const [find, setFind] = useState('');
+	useEffect(() => {
+		axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+			setCountries(response.data);
+		});
+	}, []);
+
+	const countriesDisplay =
+		find === ''
+			? countries
+			: countries.filter(country =>
+					country.name.toLowerCase().match(find.toLowerCase())
+			  );
+
+	console.log(countriesDisplay.length);
+
+	const handleFind = event => {
+		setFind(event.target.value);
+	};
+
+	return (
+		<div>
+			find countries <input value={find} onChange={handleFind} />
+			<br />
+			<ListCountry countries={countriesDisplay} />
+		</div>
+	);
+};
+
+export default App;
