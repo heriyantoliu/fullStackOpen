c0a5e068990d35d2a431cc6e24b8e9dc71c2f255
heriyanto <heriyanto.liu@sidomuncul.co.id>
2019-08-27 13:21:09 +0700

    exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Person from './components/Person';
 
 const App = () => {
 	const [persons, setPersons] = useState([
@@ -48,27 +51,17 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<div>
-				filter shown with <input onChange={handleSearchName} />
-			</div>
-			<h2>add a new</h2>
-			<form onSubmit={addPerson}>
-				<div>
-					name: <input value={newName} onChange={handleNewName} />
-				</div>
-				<div>
-					number: <input value={newNumber} onChange={handleNewNumber} />
-				</div>
-				<div>
-					<button type='submit'>add</button>
-				</div>
-			</form>
+			<Filter handleSearch={handleSearchName} />
+			<h3>add a new</h3>
+			<PersonForm
+				handleSubmit={addPerson}
+				newName={newName}
+				handleNewName={handleNewName}
+				newNumber={newNumber}
+				handleNewNumber={handleNewNumber}
+			/>
 			<h2>Numbers</h2>
-			{personToShow.map(person => (
-				<p key={person.name}>
-					{person.name} {person.number}
-				</p>
-			))}
+			<Person persons={personToShow} />
 		</div>
 	);
 };
